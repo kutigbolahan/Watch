f1ce87a76d739b78d51600eae7429594bcae3122
kuti gbolahan <kutigbolahan@ymail.com>
2020-08-14 16:14:05 +0100

    second commit

---
+++ lib/watch.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Watch extends StatefulWidget {
+  @override
+  _WatchState createState() => _WatchState();
+}
+
+class _WatchState extends State<Watch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
